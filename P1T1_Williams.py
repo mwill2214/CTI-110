0f85170e81b84ec2cf418ecd4b791391ac64690f
mwill2214 <114512002+mwill2214@users.noreply.github.com>
2022-09-27 14:23:04 -0400

    Add files via upload

---
+++ P1T1_Williams.py
@@ -0,0 +1,11 @@
+# CTI 110
+# P1T1-Hello World
+# Myra Williams
+# 30 August 2022
+
+print ("Hello World!")
+print (" Hey what's your name")
+name = input ( )
+print (" Nice to meet you, ", name)
+
+
