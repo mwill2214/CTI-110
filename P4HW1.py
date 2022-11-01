b1c1448bdbb32c6d3acfa758d159d26da584efe3
mwill2214 <mashaw6854@gmail.com>
2022-11-01 14:09:41 -0400

    Update P4HW1.py
    Fixed indented code

--- P4HW1.py
+++ P4HW1.py
@@ -5,13 +5,14 @@ Ask user for six grades,  and store them in the list called "grade "
 """
 
 grades = []
-i = 1
+
 for i in range (6):
-    num = str(i)
-    grade = int(input('Please enter grade for module' , num , ': '))
+    num = str(i + 1)
+    grade = int(input('Please enter grade for module' + num + ': '))
     grades.append(grade)
 
-    average = sum(grade) / len(grade)
-    print('The lowest grade is:', max(grade))
-    print('The highest grade is:', min(grade))
-    print('The average grade is:', min(grade))
+average = sum(grades) / len(grades)
+print('The lowest grade is:', min(grades))
+print('The highest grade is:', max(grades))
+print('The sum grade is:', sum(grades))
+print('The average grade is:', average)
